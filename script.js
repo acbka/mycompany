340143532ec7ca69e6be3130de37ee21aa30df93
acbka <vladochka.vb@gmail.com>
2020-01-18 07:13:57 +1300

    mmm

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-function menuActiveItem(){
+function menuActiveItem() {
    const menuItems = document.querySelectorAll(".items");
    menuItems.forEach(item => item.addEventListener("click", activeItem));
 }
 
-function activeItem(){
+function activeItem() {
    let menuItem = event.target;
    document.querySelector(".active").classList.remove("active");
    menuItem.classList.add("active");
    document.querySelector(".navbar-collapse").classList.remove("show");
 }
 
-function scrollMenu(){
+function scrollMenu() {
    const anchors = document.querySelectorAll('a[href*="#"]:not([href="#"]');
    let animationTime = 300;
    let framesCount = 20;
    anchors.forEach(item => {
-   item.addEventListener('click', function(e) {
-   e.preventDefault();
-   let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-// запускаем интервал, в котором
-   let scroller = setInterval(function() {
-   // считаем на сколько скроллить за 1 такт
-   let scrollBy = coordY / framesCount;
-   // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-   // и дно страницы не достигнуто
-   if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-   // то скроллим на к-во пикселей, которое соответствует одному такту
-   window.scrollBy(0, scrollBy);
-   } else {
-   // иначе добираемся до элемента и выходим из интервала
-   window.scrollTo(0, coordY);
-   clearInterval(scroller);
-   }
-   // время интервала равняется частному от времени анимации и к-ва кадров
-   }, animationTime / framesCount);
-   });
+      item.addEventListener('click', function (e) {
+         e.preventDefault();
+         let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+         // запускаем интервал, в котором
+         let scroller = setInterval(function () {
+            // считаем на сколько скроллить за 1 такт
+            let scrollBy = coordY / framesCount;
+            // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+            // и дно страницы не достигнуто
+            if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+               // то скроллим на к-во пикселей, которое соответствует одному такту
+               window.scrollBy(0, scrollBy);
+            } else {
+               // иначе добираемся до элемента и выходим из интервала
+               window.scrollTo(0, coordY);
+               clearInterval(scroller);
+            }
+            // время интервала равняется частному от времени анимации и к-ва кадров
+         }, animationTime / framesCount);
+      });
    });
 }
 
 // go to top
 let scrollButton = document.querySelector(".scroll_top");
 
-function goToTop(){
+function goToTop() {
    window.addEventListener("scroll", trackScroll);
    scrollButton.addEventListener("click", goUp);
 }
@@ -59,15 +59,15 @@ function trackScroll() {
 
 function goUp() {
    if (window.pageYOffset > 0) {
-     window.scrollBy(0, -80);
-     setTimeout(goUp, 0);
+      window.scrollBy(0, -80);
+      setTimeout(goUp, 0);
    };
    document.querySelector(".active").classList.remove("active");
    document.querySelector('a[href="#home"]').classList.add("active");
 }
 
-function formSubmit(){
-   document.querySelector("#form-submit").addEventListener("click", function(){
+function formSubmit() {
+   document.querySelector("#form-submit").addEventListener("click", function () {
       event.preventDefault();
       document.querySelector("form").reset();
    })
