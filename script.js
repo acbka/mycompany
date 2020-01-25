fb3d997be39f4d9849c3f3b6427ffbb86de33387
acbka <vladochka.vb@gmail.com>
2020-01-26 11:21:32 +1300

    scroll

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ function scrollMenu() {
             // считаем на сколько скроллить за 1 такт
             let scrollBy = coordY / framesCount;
             // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-            // и дно страницы не достигнуто  && window.innerHeight + window.pageYOffset < document.body.offsetHeight
-            if (scrollBy > window.pageYOffset - coordY) {
+            // и дно страницы не достигнуто
+            if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
                // то скроллим на к-во пикселей, которое соответствует одному такту
                window.scrollBy(0, scrollBy);
             } else {
                // иначе добираемся до элемента и выходим из интервала
-               window.scrollTo(0, coordY);
+               window.scrollTo(0, coordY); coordY = 0;
                clearInterval(scroller);
             }
             // время интервала равняется частному от времени анимации и к-ва кадров
@@ -84,5 +84,5 @@ function formSubmit() {
 
 formSubmit()
 menuActiveItem()
-scrollMenu()
+//scrollMenu()
 goToTop()
