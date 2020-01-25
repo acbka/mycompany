73d5648fb005b070c64b494c5cdac53fab359ae7
acbka <vladochka.vb@gmail.com>
2020-01-26 12:39:44 +1300

    scroll

--- script.js
+++ script.js
@@ -1,77 +1,118 @@
-function menuActiveItem() {
-   const menuItems = document.querySelectorAll(".items"); 
-   menuItems.forEach(item => item.addEventListener("click", activeItem));
-}
 
-function activeItem() {
-   let menuItem = event.target;
-   document.querySelector(".active").classList.remove("active");
-   menuItem.classList.add("active");
-   document.querySelector(".navbar-collapse").classList.remove("show");
-}
+// active menu item
 
-function scrollMenu() {
-   //const anchors = document.querySelectorAll('a[href*="#"]:not([href="#"]');
-   const anchors = document.querySelectorAll('.items');
-   let animationTime = 300;
-   let framesCount = 20;
-   anchors.forEach(item => {
-      item.addEventListener('click', function (e) {
+let menuItems = Array.from(document.querySelectorAll(".items"));
+
+let anchors = menuItems.map(el => {
+   hash = el.href.replace(/[^#]*(.*)/, '$1'); 
+   return hash;
+})
+
+let sections = anchors.map(hash => {
+   let block = document.querySelector(hash);
+   return block ;
+})
+
+let menuHeight = document.querySelector(".navbar").offsetHeight;
+let V = .1;
+
+function activeMenuItem() {
+   menuItems.forEach(elem => {
+      elem.addEventListener('click', function (e) {
          e.preventDefault();
-         let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-         // запускаем интервал, в котором
-         let scroller = setInterval(function () {
-            // считаем на сколько скроллить за 1 такт
-            let scrollBy = coordY / framesCount;
-            // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-            // и дно страницы не достигнуто   && window.innerHeight + window.pageYOffset < document.body.offsetHeight
-            if (scrollBy > window.pageYOffset - coordY) {
-               // то скроллим на к-во пикселей, которое соответствует одному такту
-               window.scrollBy(0, scrollBy);
-            } else {
-               // иначе добираемся до элемента и выходим из интервала
-               window.scrollTo(0, coordY); coordY = 0;
-               clearInterval(scroller);
+         scrollMenu(elem);
+         
+         menuItems.forEach((nl) => {
+            if (nl != this) {
+               nl.classList.remove('active');
             }
-            // время интервала равняется частному от времени анимации и к-ва кадров
-         }, animationTime / framesCount);
-      });
+         });
+
+         this.classList.add('active');
+         document.querySelector(".navbar-collapse").classList.toggle("show")
+      }, false);
    });
 }
 
-// go to top
-let scrollButton = document.querySelector(".scroll_top");
+   // scroll menu
 
-function goToTop() {
-   window.addEventListener("scroll", showScroll);
-   scrollButton.addEventListener("click", goUp);
-}
+   function scrollMenu(item) {
+      var w = window.pageYOffset;
+      hash = item.href.replace(/[^#]*(.*)/, '$1'); 
+      t = document.querySelector(hash).getBoundingClientRect().top; 
 
-function showScroll() {
-   let scrolled = window.scrollY + 200;
-   let y = document.documentElement.clientHeight;
-
-   if (scrolled > y) {
-      scrollButton.classList.add("show");
-   } else {
-      scrollButton.classList.remove("show");
-   }
-}
-
-function goUp() {
+      start = null;
    
-   moveUp();
+      requestAnimationFrame(step);
+   
+      function step(time) {
+         let menuHeight = document.querySelector(".navbar").offsetHeight;
 
-   function moveUp(){ 
-      
-      if (window.pageYOffset > 0) {
-         window.scrollBy(0, - 80); 
-         setTimeout(moveUp, 0);
+         if (start === null) start = time;
+
+         let progress = time - start;
+         r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+         window.scrollTo(0, r);
+
+         if (r != w + t) {
+            requestAnimationFrame(step)
+         } else { 
+            window.scrollTo(0, t + w - menuHeight)
+            //location.hash = hash  -  URL с хэшем если меню не fixed
+         }
       };
    }
-  
-   document.querySelector(".active").classList.remove("active"); 
-   document.querySelector('a[href="#home"]').classList.add("active"); 
+
+   
+   // scroll page
+
+function scrollPage(){
+   let menuItems = Array.from(document.querySelectorAll(".items"));
+   window.addEventListener('scroll', activeBlock);
+
+   function activeBlock(){
+      sections.forEach(item => {
+         let start = item.offsetTop - menuHeight;
+         let end = item.offsetTop + item.offsetHeight/1.3;
+
+         if (pageYOffset > start && pageYOffset < end){
+            document.querySelector(".active").classList.remove("active");
+            let index = sections.indexOf(item);
+            menuItems[index].classList.add("active");
+         }
+      })
+   }
+}
+
+
+// go to top
+
+
+function goToTop(){
+   let scrollButton = document.querySelector(".scroll_top");
+
+   window.addEventListener("scroll", trackScroll);
+   scrollButton.addEventListener("click", goUp);
+
+   function trackScroll() {
+      let scrolled = window.scrollY + 200;
+      let y = document.documentElement.clientHeight;
+   
+      if (scrolled > y) {
+         scrollButton.classList.add("show");
+      } else {
+         scrollButton.classList.remove("show");
+      }
+   }
+
+   function goUp() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(goUp, 0);
+      };
+      document.querySelector(".active").classList.remove("active");
+      document.querySelector('[href="#home"]').classList.add("active")
+   }
 }
 
 function formSubmit() {
@@ -82,7 +123,6 @@ function formSubmit() {
 }
 
 
-formSubmit()
-menuActiveItem()
-scrollMenu()
+activeMenuItem()
 goToTop()
+formSubmit()
